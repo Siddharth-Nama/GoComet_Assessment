828bc62fb508b60fed5682ff47e5a496cd420f13
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-02-12 02:10:45 +0530

    feat: Configure LocMemCache for development

--- backend/leaderboard_project/settings.py
+++ backend/leaderboard_project/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
