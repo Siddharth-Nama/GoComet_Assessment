addef9e49914ba79f1e9fd8c605b978d62861e37
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-02-12 01:49:20 +0530

    feat: Create serializers for models and score submission

---
+++ backend/core/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import User, GameSession, Leaderboard
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'join_date']
+
+class GameSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameSession
+        fields = ['user', 'score', 'game_mode', 'timestamp']
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+
+    class Meta:
+        model = Leaderboard
+        fields = ['user', 'username', 'total_score', 'rank']
+
+class SubmitScoreSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    score = serializers.IntegerField()
