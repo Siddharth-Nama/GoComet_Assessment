dde6190f28477dc6d2d3e35ce73924e8814091eb
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-02-12 01:45:11 +0530

    feat: Define GameSession model related to User

--- backend/core/models.py
+++ backend/core/models.py
@@ -6,3 +6,12 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+class GameSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    game_mode = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.score}"
