3d85ab9b42ae0d09ab6f7c2088c61a94ed237692
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-02-12 14:17:20 +0530

    feat: Switch to Super Mario theme - Add Sky/Cloud 3D background

--- frontend/src/components/Background3D.jsx
+++ frontend/src/components/Background3D.jsx
@@ -1,37 +1,31 @@
 import React, { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Points, PointMaterial } from '@react-three/drei';
-import * as random from 'maath/random/dist/maath-random.esm';
+import { Cloud, Sky } from '@react-three/drei';
 
-const Stars = (props) => {
+const Clouds = () => {
   const ref = useRef();
-  const [sphere] = React.useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }));
-
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+    ref.current.rotation.y += delta / 20;
   });
-
   return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
-        <PointMaterial
-          transparent
-          color="#ff0000" // Red user requested
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
+    <group ref={ref}>
+      <Cloud position={[-4, -2, -25]} speed={0.2} opacity={1} />
+      <Cloud position={[4, 2, -15]} speed={0.2} opacity={0.5} />
+      <Cloud position={[-4, 2, -10]} speed={0.2} opacity={1} />
+      <Cloud position={[4, -2, -5]} speed={0.2} opacity={0.5} />
+      <Cloud position={[4, 2, 0]} speed={0.2} opacity={0.75} />
     </group>
   );
 };
 
 const Background3D = () => {
     return (
-        <div className="fixed inset-0 -z-10 bg-black">
-            <Canvas camera={{ position: [0, 0, 1] }}>
-                <Stars />
+        <div className="fixed inset-0 -z-10 bg-[#5c94fc]">
+            <Canvas camera={{ position: [0, 0, 5] }}>
+                <Sky sunPosition={[100, 20, 100]} turbidity={0.1} rayleigh={0.5} mieCoefficient={0.005} mieDirectionalG={0.8} />
+                <ambientLight intensity={0.8} />
+                <pointLight position={[100, 100, 100]} intensity={1} />
+                <Clouds />
             </Canvas>
         </div>
     );
